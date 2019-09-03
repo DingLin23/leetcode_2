64180b2a469f29efa82c7e791c3509fea3af3056
Ding Lin <tim920323@gmail.com>
2019-09-03 14:14:26 -0400

    fix

--- Array/TrappingRainWater.java
+++ Array/TrappingRainWater.java
@@ -1,33 +1,39 @@
-public class solution{
-public int trap(int[] height) {
-    if (height == null || height.length == 0) {
-        return 0;
-    }
+public class TrappingRainWater {
+    /**
+     *
 
-    int result = 0;
-    // leftMax represents the highest bar from left
-    int leftMax = Integer.MIN_VALUE;
-    // rightMax represents the highest bar from right
-    int rightMax = Integer.MIN_VALUE;
+     0,1,0,2,1,0,1,3,2,1,2,1
 
-    // use two pointers to scan the entire array until they meet with each other
-    // Key points: any bars in the middle of leftMax bar and rightMax bar will not influence
-    // how much water can current position trap
-    for (int left = 0, right = height.length - 1; left <= right;) {
-        leftMax = Math.max(leftMax, height[left]);
-        rightMax = Math.max(rightMax, height[right]);
-        
-        //how much can current position trap depends on the shorter bar (木桶原理)
-        if (leftMax < rightMax) {
-            //DO NOT FORGET to subtract bar height of current position
-            result += leftMax - height[left];
-            left++;
+                   *
+           *       * *   *
+       *   * *   * * * * * *
+     0 1 2 3 4 5 6 7 8 9 0 1
+                 l r
+
+     time : O(n)
+     space : O(1)
+
+     *
+     * @param height
+     * @return
+     */
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int res = 0;
+        int leftMax = 0;
+        int rightMax = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(height[left], leftMax);
+                res += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = Math.max(height[right], rightMax);
+                res += rightMax - height[right];
+                right--;
+            }
         }
-        else {
-            result += rightMax - height[right];
-            right--;
-        }
+        return res;
     }
-    return result;
-}
 }
