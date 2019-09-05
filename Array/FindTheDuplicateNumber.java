f213a697d442aa9d5540249c235382d11575ec09
Ding Lin <tim920323@gmail.com>
2019-09-05 18:38:13 -0400

    Q287

---
+++ Array/FindTheDuplicateNumber.java
@@ -0,0 +1,22 @@
+//time:o(n)
+//space:o(n)
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+         int slow = nums[0];
+         int fast = nums[nums[0]];
+        while(slow!= fast){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+       fast = 0;
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return fast;
+        
+       
+ }
+ 
+}
