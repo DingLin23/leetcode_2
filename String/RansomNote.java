0111dd1e454aa157d59194e48d29bf099159c61e
Ding Lin <tim920323@gmail.com>
2019-09-19 13:08:33 -0400

    Q383

---
+++ String/RansomNote.java
@@ -0,0 +1,28 @@
+public class RansomNote {
+    /**
+     * You may assume that both strings contain only lowercase letters.
+
+     canConstruct("a", "b") -> false
+     canConstruct("aa", "ab") -> false
+     canConstruct("aa", "aab") -> true
+
+     time : O(n)
+     space : O(1)
+
+     * @param ransomNote
+     * @param magazine
+     * @return
+     */
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (--count[ransomNote.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
