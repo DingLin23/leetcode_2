d5463ccae8b83473d44be2f91fb51fa755534d08
Ding Lin <tim920323@gmail.com>
2019-09-19 21:52:40 -0400

    q205

---
+++ String/IsomorphicString.java
@@ -0,0 +1,18 @@
+//time: o(n)
+//space: o(n)
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] sChars = new int[256];
+        int[] tChars = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            if (sChars[s.charAt(i)] != tChars[t.charAt(i)]) {
+                return false;
+            } else {
+                sChars[s.charAt(i)] = tChars[t.charAt(i)] = t.charAt(i);
+            }
+        }
+        return true;
+    }
+}
