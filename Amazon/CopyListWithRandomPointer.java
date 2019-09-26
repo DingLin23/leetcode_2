ca747c31532f1a982075fb44c49dd9c929c4d070
Ding Lin <tim920323@gmail.com>
2019-09-25 20:25:44 -0400

    q138

---
+++ Amazon/CopyListWithRandomPointer.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
+// time:o(n)
+//space:o(n)
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        HashMap<Node,Node> map = new HashMap<>();
+        Node cur = head;
+        while(cur != null){
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+        }
+}
