cef3366fce5ea8dd920103a348077e7bb09b948c
Ding Lin <tim920323@gmail.com>
2019-10-16 15:27:29 -0400

    q866

---
+++ AmeicanExpress/oa1.java
@@ -0,0 +1,8 @@
+public class oa1{
+    public int solution(int A, int B){
+        int [] arry = new int[(B-A)+1];
+        for(int i = A, i < arry.length; i++){
+            if
+        }
+    }
+}
