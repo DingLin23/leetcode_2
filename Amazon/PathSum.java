588b3d763e6230da6f2ff46fa8e1707a3d162a1c
dinglin <tim920323@gmail.com>
2019-11-20 17:32:03 -0500

    看看

--- Amazon/PathSum.java
+++ Amazon/PathSum.java
@@ -7,11 +7,35 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// time : O(n);
+// space : O(n);
 class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if(root == null) return false;// to detect root is null or giving an option for later excuting if either one of root.left and root.right is null.
         if(root.left == null && root.right == null) return sum == root.val;
         //checking when u hitting each path to the end.
         return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val); // excuting the one is true; 
+    }
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode cur = root;
+            cur = stack.pop();
+            if(cur.left == null && cur.right == null){
+                if(sum == cur.val) return true;
+            }
+            if(cur.left != null) {
+                stack.push(cur.left);
+                cur.left.val += cur.val;
+            }
+            if(cur.right != null) {
+                stack.push(cur.right);
+                cur.right.val += cur.val;
+            }
+
+        }
+        return false;
     }
 }
