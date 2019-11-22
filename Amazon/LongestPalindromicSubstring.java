63a7484c51b51592a35802e142fb1de2d112080f
dinglin <tim920323@gmail.com>
2019-11-21 19:27:24 -0500

    kk

--- Amazon/LongestPalindromicSubstring.java
+++ Amazon/LongestPalindromicSubstring.java
@@ -1,21 +1,49 @@
 //time:O(n^2)
 //space:O(n^2)
-
 class Solution {
     public String longestPalindrome(String s) {
-       int n = s.length();
-        String res = "";
-        boolean [][] dp = new boolean[n][n];
-        for(int i = n; i>= 0; i--){
-            for(int j = i; j <n; j++){
-                dp[i][j] = s.charAt(i) == s.charAt(j) && (j-i < 3 || dp[i+1][j-1]);
-                if (dp[i][j] && (res == null || j-i+1>res.length())){
-                    res= s.substring(i, j+1);
+        int n = s.length();
+        String res = null;
+        int palindromeStartsAt = 0, maxLen = 0;
+
+        boolean[][] dp = new boolean[n][n];
+        // dp[i][j] indicates whether substring s starting at index i and ending at j is palindrome
+        
+        for(int i = n-1; i >= 0; i--) { // keep increasing the possible palindrome string
+            for(int j = i; j < n; j++) { // find the max palindrome within this window of (i,j)
+                
+                //check if substring between (i,j) is palindrome
+                dp[i][j] = (s.charAt(i) == s.charAt(j)) // chars at i and j should match
+                           && 
+                           ( j-i < 3  // if window is less than or equal to 3, just end chars should match
+                             || dp[i+1][j-1]  ); // if window is > 3, substring (i+1, j-1) should be palindrome too
+                
+                //update max palindrome string
+                if(dp[i][j] && (j-i+1 > maxLen)) {
+                    palindromeStartsAt = i;
+                    maxLen = j-i+1;
                 }
             }
         }
+        return s.substring(palindromeStartsAt, palindromeStartsAt+maxLen);
+    }
+// time : O(n^2) space : O(1)
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return s;
+        for (int i = 0; i < s.length(); i++) {
+            helper(s, i, i);
+            helper(s, i, i + 1);
+        }
         return res;
-        
-        
+    }
+    public void helper(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        String cur = s.substring(left + 1, right);
+        if (cur.length() > res.length()) {
+            res = cur;
+        }
     }
 }
