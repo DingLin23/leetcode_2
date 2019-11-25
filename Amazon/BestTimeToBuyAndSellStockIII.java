20c340b932ed5fa5742c74ee67c1fd2574f29fef
dinglin <tim920323@gmail.com>
2019-11-25 15:14:28 -0500

    123

---
+++ Amazon/BestTimeToBuyAndSellStockIII.java
@@ -0,0 +1,17 @@
+//time:o(n)
+//space:o(1)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buyOne = Integer.MAX_VALUE;
+        int sellOne = 0;
+        int buyTwo = Integer.MAX_VALUE;
+        int sellTwo = 0;
+        for(int price : prices){
+            buyOne = Math.min(buyOne, price);
+            sellOne = Math.max(sellOne,price-buyOne);
+            buyTwo = Math.min(buyTwo, price-sellOne);
+            sellTwo = Math.max(sellTwo, price-buyTwo);
+        }
+        return sellTwo;
+    }
+}
