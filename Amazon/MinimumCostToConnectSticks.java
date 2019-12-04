9b3379a6a08146484ce60f67767a6b0c8470d123
dinglin <tim920323@gmail.com>
2019-12-04 16:43:51 -0500

    correct

--- Amazon/MinimumCostToConnectSticks.java
+++ Amazon/MinimumCostToConnectSticks.java
@@ -1,16 +1,17 @@
 //time:O(nlg(n))
 // space:O(n)
+//yes
 class Solution {    
     public int connectSticks(int[] sticks) {
         Queue<Integer> pq = new PriorityQueue<Integer>(); //自动排序升序。
         for(int i: sticks) pq.add(i);
-        int res = 0;
+        int ans = 0;
         while(pq.size() > 1){
             int t = pq.poll() + pq.poll();   // sum the two smallest 
-            res +=t;  // count the cost
+            ans +=t;  // count the cost
             pq.offer(t); //一个queue里面可以放一个temp的指针去临时存储
         }
-        return res;
+        return ans;
     
     }
     
