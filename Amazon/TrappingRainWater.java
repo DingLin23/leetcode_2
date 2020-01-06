db8bf13e2dad2e44c45fb435715dcd094df202a7
dinglin <tim920323@gmail.com>
2020-01-05 21:41:19 -0500

    cool

--- Amazon/TrappingRainWater.java
+++ Amazon/TrappingRainWater.java
@@ -12,10 +12,12 @@ class Solution {
         int rightMax = 0;
         while (left < right) {
             if (height[left] < height[right]) {
+                //from compare with preleft from left to right if val is different then subtract to get res.
                 leftMax = Math.max(height[left], leftMax); // 总是大的值减去每个对应的小的值。！！
                 res += leftMax - height[left];
                 left++;
             } else {
+             //from compare with preright from right to left if val is different then subtract to get res.
                 rightMax = Math.max(height[right], rightMax);
                 res += rightMax - height[right];
                 right--;
