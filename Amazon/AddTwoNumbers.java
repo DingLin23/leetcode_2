7e969db9d3ba32942189955fd34b3d9857bf894e
dinglin <tim920323@gmail.com>
2020-01-09 11:24:42 -0500

    wawa

--- Amazon/AddTwoNumbers.java
+++ Amazon/AddTwoNumbers.java
@@ -12,27 +12,28 @@ public class AddTwoNumbers {
      * @return
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode (0); // dummy in order to return from the beginning node
+        ListNode cur = dummy; // a pointer to traverse
         int sum = 0;
-        ListNode cur = dummy;
         ListNode p1 = l1, p2 = l2;
-        while (p1 != null || p2 != null) {
-            if (p1 != null) {
+        while(p1 != null || p2 != null){
+            if(p1 != null){
                 sum += p1.val;
                 p1 = p1.next;
             }
-            if (p2 != null) {
+            if(p2 != null){
                 sum += p2.val;
                 p2 = p2.next;
             }
-            cur.next = new ListNode(sum % 10);//如果大于10，留余数，如果小于10取原数
-            sum /= 10;//carring the 1 to the next number.
+            cur.next = new ListNode(sum%10); //if sum < 10, if > 10 cur.next is the remainder.
+            sum /= 10; // if sum < 10, sum update to 0, else taking the carries 1.
             cur = cur.next;
         }
-        if (sum == 1) { //if the last two add pair has number greater than 10
-            cur.next = new ListNode(1); // carring the 1 to the next new ndoe
+        // if the last sum is > 10;
+        if(sum == 1){
+            cur.next = new ListNode(1);
         }
-        return dummy.next;
+         return dummy.next;
     }
 
     public static void main(String[] args) {
