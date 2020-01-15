1a2359068f473b6d64d11a46ef6614bb1cf3c4a4
dinglin <tim920323@gmail.com>
2020-01-15 14:53:55 -0500

    waaw

---
+++ Amazon/wordLadder.java
@@ -0,0 +1,36 @@
+class Solution {
+    //counts transformations times.
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        int level = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();// must to have this, becasue queue.size()will change, we need to record size for that time.
+            for(int i = 0; i < size; i++){
+                String cur = queue.remove();
+                char[] wordUnit = cur.toCharArray();
+                if(cur.equals(endWord)){
+                    return level+1;
+                }
+                for(int j = 0; j < cur.length();j++){
+                    char temp = wordUnit[j];//store the original wordUnit.
+                    for(char c ='a'; c<='z';c++){
+                        wordUnit[j] = c;
+                        String s = new String(wordUnit);
+                        if(set.contains(s)){
+                            queue.add(s);
+                            set.remove(s);
+                        }
+                    }
+                    wordUnit[j] = temp;//if the first letter changes not found, get back to the original wordUnit.
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+}
+
+
+
