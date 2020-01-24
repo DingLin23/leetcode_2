25d4ba38c652c89b329f6bc57e1221c085556455
dinglin <tim920323@gmail.com>
2020-01-23 20:37:14 -0500

    waaa

--- Amazon/BinaryTreeLevelOrderTraversal.java
+++ Amazon/BinaryTreeLevelOrderTraversal.java
@@ -22,3 +22,22 @@ class Solution {
         return res;
         }
 }
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+        helper(root, 0, res);
+        return res;
+    }
+    private void helper(TreeNode root, int level, List<List<Integer>>res){
+        if(root == null) return;
+        if(res.size() <= level){
+            res.add(new ArrayList<>());
+        }
+        res.get(level).add(root.val);
+        helper(root.left, level+1, res);
+        helper(root.right,level+1,res);
+    }
+}
+
