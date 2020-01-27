563c3cbcf194a49318344fa043e21b9fd976623a
dinglin <tim920323@gmail.com>
2020-01-27 16:42:34 -0500

    waaw

---
+++ Amazon/ BinaryTreePreOrderTraversal.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+ //time:o(N)
+ //space;o(n)
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode cur = stack.pop();
+            res.add(cur.val);
+            if(cur.right != null){
+                stack.push(cur.right);
+            }
+            if(cur.left != null){
+                stack.push(cur.left);
+            }
+        }
+        return res;
+    }
+
+    //time:O(N)
+    //SPACE:O(1)
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+        helper(root, res);
+        return root;
+    }
+    private void helper(TreeNode root, List<Integer> res){
+        if(root == null) return;
+        res.add(root.val);
+        helper(root.left,res);
+        helper(root.right,res);
+    }
+}
