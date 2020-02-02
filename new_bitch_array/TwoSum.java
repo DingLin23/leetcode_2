a8771fa2d8e82649f3735fccb467f656435d493a
dinglin <tim920323@gmail.com>
2020-02-02 16:13:01 -0500

    dsadas

---
+++ new_bitch_array/TwoSum.java
@@ -0,0 +1,25 @@
+//time:O(n)
+//space:O(n)
+import java.util.*;
+public class TwoSum{
+public static int[] Ts(int[] nums, final int target){
+    int [] res = new int[2];
+    if(nums == null || nums.length < 2) return res;
+    Map<Integer,Integer> map = new HashMap<>();
+    for(int i = 0; i < nums.length; i++){
+        if(map.containsKey(target - nums[i])){
+            res[0] = map.get(target-nums[i]);
+            res[1] = i;
+        }
+        map.put(nums[i],i);
+    }
+    return res;
+}
+
+    public static void main(String[] args) {
+        int nums[] = { 1, 4, 6, 3 };
+        int target = 7;
+        int res []=Ts(nums, target);
+        System.out.println(target);
+}
+}
