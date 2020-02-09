ba197369cfc982883c204ca3f3fe14bc71f19801
dinglin <tim920323@gmail.com>
2020-02-09 17:11:04 -0500

    814

---
+++ CapitalOne/BinaryTreePruning.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+ //time: o(n)
+ //space: o(1)
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if(root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if(root.left == null && root.right == null && root.val == 0) return null;
+        else return root;
+    }
+}
