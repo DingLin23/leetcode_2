58165b8603d8591b7bbb89884f8e26c3d1a82a34
dinglin <tim920323@gmail.com>
2020-02-11 12:31:08 -0500

    das

--- Amazon/GroupAnagrams.java
+++ Amazon/GroupAnagrams.java
@@ -15,9 +15,9 @@
 // The order of your output does not matter.
 
 
-algo: 1.we gonna change the str in str change into char array in order to sort it and  change it back to String type put it to hashmap 
-      2. check if hashmap contains same sorted str then we add the original string to the sublist which the same one located
-      3. else we create a new list to store original string and add this list into res list.
+// algo: 1.we gonna change the str in str change into char array in order to sort it and  change it back to String type put it to hashmap 
+//       2. check if hashmap contains same sorted str then we add the original string to the sublist which the same one located
+//       3. else we create a new list to store original string and add this list into res list.
 
 //Time Complexity: O(MNlogN) M is the length of strs, and N is the maximum length of a string in strs
 //space: O(MN)
