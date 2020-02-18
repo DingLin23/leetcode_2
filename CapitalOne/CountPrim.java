0f2292f1655c2b30c871e9245d3f89dc9bfadbdd
dinglin <tim920323@gmail.com>
2020-02-18 12:28:38 -0500

    q204

---
+++ CapitalOne/CountPrim.java
@@ -0,0 +1,18 @@
+//time:o(nlglgn)
+//space:o(n)
+
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int res = 0;
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i] == false) {
+                res++;
+                for (int j = 2; i * j < n; j++) {
+                    notPrime[i * j] = true;
+                }
+            }
+        }
+        return res;
+    }
+}
