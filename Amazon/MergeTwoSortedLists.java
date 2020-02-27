7787d53d5f2736a3524556e4826f6909b8d820c5
dinglin <tim920323@gmail.com>
2020-02-27 16:41:43 -0500

    dsad

--- Amazon/MergeTwoSortedLists.java
+++ Amazon/MergeTwoSortedLists.java
@@ -30,6 +30,7 @@ class Solution {
 
    // recursive way
         //time:o(n); space:o(1)
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null) return l2;
         if(l2 == null) return l1;
         if(l1.val < l2.val){
@@ -39,4 +40,5 @@ class Solution {
             l2.next = mergeTwoLists(l1, l2.next);
             return l2;
         }
+    }
 }
