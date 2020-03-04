c3c809bbb6b1ce9bb0712fb164a2953516da0709
dinglin <tim920323@gmail.com>
2020-03-03 19:42:39 -0500

    dsada

--- CapitalOne/MaximumSubarray.java
+++ CapitalOne/MaximumSubarray.java
@@ -4,8 +4,8 @@ class Solution {
         int sum = nums[0];
         int res = nums[0];
         for(int i = 1; i < nums.length; i++){
-            sum = Math.max(nums[i], sum+nums[i]);
-            res = Math.max(res,sum);
+            sum = Math.max(nums[i], sum+nums[i]); // compare cur postion to the sum of previous position.
+            res = Math.max(res,sum);// update the result.
         }
         return res;
     }
