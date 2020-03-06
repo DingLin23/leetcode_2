86ca65c36f2fe3656ec72251755cdbadfcb5cb71
dinglin <tim920323@gmail.com>
2020-03-05 22:16:25 -0500

    dsadas

--- CapitalOne/ValidAnagram.java
+++ CapitalOne/ValidAnagram.java
@@ -12,3 +12,15 @@ class Solution {
         return Arrays.equals(sChar, tChar);   
     }
 }
+
+//time: o(n)
+//space: o(n)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
+        return true;
+    }
+}
