d6f81d15e29b7819ef15b065042e14af70b3eebf
dinglin <tim920323@gmail.com>
2020-03-05 21:04:43 -0500

    dasda

--- CapitalOne/CointChange.java
+++ CapitalOne/CointChange.java
@@ -2,20 +2,22 @@
 //space: o(amount)
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0) return 0;// amount = 0, no coins can be made up
-        if (coins == null || coins.length == 0) return -1;// no coins return -1;
-
-        int[] dp = new int[amount + 1];// means total coints can be made up by that amount of money
-        for (int i = 1; i <= amount; i++) {
-            int min = Integer.MAX_VALUE;
-            for (int j = 0; j < coins.length; j++) {
-                // amount gonna greater than coint,dp[i - coins[j]] != -1 after this coint still able to make up as new coint.
-                if (i >= coins[j] && dp[i - coins[j]] != -1) {
-                    min = Math.min(min, dp[i - coins[j]] + 1);//dp[i - coins[j]] + 1 coint count +1;
+        if(amount == 0) return 0; //corner case
+        if(coins == null || coins.length == 0) return -1;
+        int dp[] = new int[amount+1];//dp[] amount of coint can make up of the amount of money.
+        for(int i = 1; i <= amount; i++){ // amount = 0 is covered, make up to the amount.
+            int min = Integer.MAX_VALUE; 
+            for(int j = 0; j < coins.length; j++){ //looping the coints
+                
+                //i should be greate and equal to that coint amount and after took this coint and the rest of amount able to make up with other coint.
+                if(i >= coins[j] && dp[i-coins[j]] != -1){
+                    // memorized the dp[i-coins[j]]+1;
+                    min = Math.min(min,dp[i-coins[j]]+1);
                 }
             }
-            dp[i] = min == Integer.MAX_VALUE ? -1 : min;
+            
+            dp[i] = min == Integer.MAX_VALUE? -1 : min;
         }
-        return dp[amount];    
+        return dp[amount];
     }
 }
