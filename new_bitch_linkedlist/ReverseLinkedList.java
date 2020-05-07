b0200132e69a5f203e5a668fcfbc35971ec7ad55
dinglin <tim920323@gmail.com>
2020-05-07 18:23:07 -0400

    206

---
+++ new_bitch_linkedlist/ReverseLinkedList.java
@@ -0,0 +1,20 @@
+// Reverse a singly linked list.
+
+// Example:
+
+// Input: 1->2->3->4->5->NULL
+// Output: 5->4->3->2->1->NULL
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null) return null;
+        ListNode pre = head, cur = head.next, next = null;
+        while(cur != null){
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        head.next = null;
+        return pre;
+    }
+}
