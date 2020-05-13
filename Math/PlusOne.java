70296e1b48fb631e1a528e7826746f47f3effd82
dinglin <tim920323@gmail.com>
2020-05-13 14:55:31 -0400

    wow

---
+++ Math/PlusOne.java
@@ -0,0 +1,21 @@
+//time:o(n)
+//spcae:o(n)
+
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        if (digits == null || digits.length == 0) return digits;
+        
+        for (int i = digits.length -1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }else{
+                digits[i] = 0;
+            }
+        }
+        
+        int res[] = new int[digits.length+1];
+        res[0] = 1;
+        return res;
+    }
+}
