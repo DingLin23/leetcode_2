98ff249fce0806c5a2fb21aebd83ebe479d24068
dinglin <tim920323@gmail.com>
2020-05-28 10:11:06 -0400

    saS

--- new_bitch_array/SubarraySumEqualsK.java
+++ new_bitch_array/SubarraySumEqualsK.java
@@ -44,7 +44,7 @@ class Solution2 {
 
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j <= nums.length; j++) {
-                if(k == nums[end]-nums[start]) count++;
+                if(k == sum[end]-sum[start]) count++;
             }
         }
         return count;
