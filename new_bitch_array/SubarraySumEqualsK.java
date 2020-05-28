1e52db936ef6e9ba992ca264ba70e0e4879c0956
dinglin <tim920323@gmail.com>
2020-05-28 14:02:19 -0400

    dsad

--- new_bitch_array/SubarraySumEqualsK.java
+++ new_bitch_array/SubarraySumEqualsK.java
@@ -67,3 +67,22 @@ class Solution2 {
         return count;
     }
 }
+
+
+//hashmap 
+//time:o(n)
+//space: o(n)
+public class Solution3 {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        map.put(0, 1); //  就是sum-k == 0 等于key的话，就get value 1. which count+1;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
