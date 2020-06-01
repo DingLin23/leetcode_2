3505583f7826d91fe663d28662c7a8974440d76e
dinglin <tim920323@gmail.com>
2020-06-01 17:07:14 -0400

    dsad

--- Tree/InvertBinaryTree.java
+++ Tree/InvertBinaryTree.java
@@ -25,3 +25,21 @@ class Solution {
         return root;
     }
 }
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+        while (!q.isEmpty()) {
+            TreeNode cur = q.poll();
+            TreeNode temp = cur.left;
+            if (cur.left != null) q.add(cur.left);
+            if (cur.right != null) q.add(cur.right);
+            cur.left = cur.right;
+            cur.right = temp;
+        }
+        return root;
+    }
+}
