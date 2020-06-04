3958b10934c59f742bc01a7387a374499fea8ba0
dinglin <tim920323@gmail.com>
2020-06-04 02:56:59 -0400

    dsada

--- Amazon/NumberOfIslands.java
+++ Amazon/NumberOfIslands.java
@@ -64,3 +64,43 @@ class Solution {
         }
     }
 }
+
+
+
+// Time Complexity: O(M X N)
+// Space Complexity: O(min(M,N))
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        if (grid == null || grid.length == 0) return count;
+        int [][] dirs = {{0,-1},{0,1},{1,0},{-1,0}};
+        
+        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    q.add(new Pair<>(i,j));
+                    while (!q.isEmpty()) {
+                        Pair<Integer,Integer> rc = q.poll();
+                        for (int[] dir : dirs) {
+                            int row = rc.getKey()+dir[0];
+                            int col = rc.getValue()+dir[1];
+                        
+                        if (row >= 0
+                               && row < grid.length
+                               && col >= 0
+                               && col < grid[0].length
+                               && grid[row][col] == '1') {
+                               q.add(new Pair<>(row, col));
+                                grid[row][col] = '0';
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
