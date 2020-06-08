7098331f04d9bb50d1c7b05365c7db4c9480d415
dinglin <tim920323@gmail.com>
2020-06-07 21:07:35 -0400

    dasda

---
+++ DP/CoinChange2.java
@@ -0,0 +1,15 @@
+//time: O(n*amount)
+//space:O(amount)
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+}
