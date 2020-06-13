65a14517b0b87d445e19202611df1f6f9f0e95bf
dinglin <tim920323@gmail.com>
2020-06-12 21:17:03 -0400

    sADA

--- BinarySearch/SearchInsertPosition.java
+++ BinarySearch/SearchInsertPosition.java
@@ -25,4 +25,24 @@ class Solution {
         }
         return left;
     }
+
+
+//time:o(nlgn)
+//space:o(1)
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int low = 1, high = nums.length - 1;
+    while (low <= high) {
+        int mid = (low + high) >>> 1;
+        int cnt = 0;
+        for (int a : nums) {
+            if (a <= mid) ++cnt;
+        }
+        if (cnt <= mid) low = mid + 1;
+        else high = mid-1;
+    }
+    return low;
+    }
+        
 }
