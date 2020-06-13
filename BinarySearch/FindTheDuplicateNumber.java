bf4f4042d8b76884ad22abcfa9e48f0d11935269
dinglin <tim920323@gmail.com>
2020-06-13 17:12:00 -0400

    dsadas

--- BinarySearch/FindTheDuplicateNumber.java
+++ BinarySearch/FindTheDuplicateNumber.java
@@ -25,3 +25,51 @@
             return res;
         }
     }
+
+//time:O(nlgn)
+//space:o(1)
+    class Solution {
+        public int findDuplicate(int[] nums) {
+            int low = 1, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int cnt = 0;
+            for (int a : nums) {
+                if (a <= mid) ++cnt;
+            }
+            if (cnt <= mid) low = mid + 1;
+            else high = mid-1;
+        }
+        return low;
+        }
+            
+    }
+
+//Time:o(n)
+//space:o(1)
+//find a circle
+
+    class Solution {
+        public int findDuplicate(int[] nums) {
+          if (nums.length > 1)
+        {
+            int slow = nums[0];
+            int fast = nums[nums[0]];
+            while (slow != fast)
+            {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+    
+            fast = 0;
+            while (fast != slow)
+            {
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+            return slow;
+        }
+        return -1;  
+        }
+            
+    }
