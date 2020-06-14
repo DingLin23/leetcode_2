468a890c6404f81358f1c7425b0582be93569a6d
dinglin <tim920323@gmail.com>
2020-06-14 19:31:34 -0400

    dsad

--- new_bitch_array/MeetingRoom.java
+++ new_bitch_array/MeetingRoom.java
@@ -1,3 +1,13 @@
-public class MeetingRoom {
-    
+//time: O(nlgn)
+//space:O(1)
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals,(int[]a, int[]b) -> a[0]-b[0]);
+        for(int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i-1][1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
