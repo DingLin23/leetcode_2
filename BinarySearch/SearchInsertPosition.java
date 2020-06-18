93b5d27f458f6b6d2554c9d69d4e48d76c33134f
dinglin <tim920323@gmail.com>
2020-06-18 03:09:55 -0400

    大大

--- BinarySearch/SearchInsertPosition.java
+++ BinarySearch/SearchInsertPosition.java
@@ -46,3 +46,25 @@ class Solution {
     }
         
 }
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return 0;
+        int left = 0, right = nums.length-1;
+        
+        while (left+1 < right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                right = mid;
+            }else {
+                left = mid;
+            }
+        }
+        if (nums[left] >= target) return left; // 边界问题
+        else if (nums[right] >= target) return right;
+        return right+1;
+    }
+}
