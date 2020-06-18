eeb9c04facf2b35315e1b7869753997ef0eb0bfa
dinglin <tim920323@gmail.com>
2020-06-18 03:25:10 -0400

    dada

--- BinarySearch/FindFirstAndLastPositionOfElementInSortedArray.java
+++ BinarySearch/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -3,51 +3,52 @@
 
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[2];
+        int res[] = new int[2];
         res[0] = -1;
         res[1] = -1;
+        if (nums.length == 0 || nums == null) return res;
+        int left = 0, right = nums.length-1;
         
-        if(nums == null || nums.length == 0) return res;
+        res[0] = findFirst(nums,target,left,right);
+        res[1] = findLast(nums,target,left,right);
+        return res;           
+        }
         
-        res[0] = findFirst(nums,target);
-        res[1] = findLast(nums,target);
-        return res;
-           
-    }
-    private int findFirst(int[] nums, int target){
-        int start = 0, end = nums.length -1;
-   
-        while(start+1 < end){
-            int mid = start + (end-start)/2;
+        private int findFirst(int[] nums, int target, int left, int right) {
+            while (left+1 < right) {
+            int mid = left + (right - left)/2;
                 // if (nums[mid] == target) {  // 不可以这么写，因为，找到后，还要往前，或则往后找。
                 //     return mid;
                 // }
-            if(nums[mid] >= target){
-                end = mid;
-            }else{
-                start = mid;
+                if (nums[mid] >= target) { // duplicate staff, 找到mid了后，继续往左二分。
+                    right = mid;
+                }else {
+                    left = mid;
+                }
             }
+            
+            if (nums[left] == target) return left;
+            else if (nums[right] == target) return right;
+            else return -1;
         }
-        if(nums[start] == target) return start;
-        else if(nums[end] == target) return end;
-        return -1;
-    }
-    private int findLast(int [] nums, int target){
-        int start = 0, end = nums.length -1;
-   
-        while(start+1<end){
-            int mid = start + (end-start)/2;
-            if(nums[mid] <= target){
-                start = mid;
-            }else{
-                end = mid;
-            }
-        }
+        
+        private int findLast(int[] nums, int target, int left, int right) {
+            while (left+1 < right) {
+            int mid = left + (right - left)/2;
 
-        if(nums[end] == target) return end;
-        else if(nums[start] == target) return start;
-        return -1;
-    }
+                if (nums[mid] <= target) { // duplicate staff, 找到mid了后，继续往右二分。
+                    left = mid;
+                }else {
+                    right = mid;
+                }
+            }
+            
+            if (nums[right] == target) return right;//必须要先输出right，因为left index 在findFirst先找到了。 if has input[2,2] will be wrong, if return left first;
+            else if (nums[left] == target) return left;
+            else return -1;
+        } 
 }
+
+
 
 
