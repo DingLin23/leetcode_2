48cf6f392503cf99d73ce69db80fded7cb5e8f68
dinglin <tim920323@gmail.com>
2020-06-20 20:10:06 -0400

    dsda

---
+++ new_bitch_str/ExcelSheetColumnNumber.java
@@ -0,0 +1,11 @@
+//time:o(n)
+//space:o(1)
+class Solution {
+    public int titleToNumber(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            res = res * 26 + (s.charAt(i)-'A'+1);
+        }
+        return res;
+    }
+}
