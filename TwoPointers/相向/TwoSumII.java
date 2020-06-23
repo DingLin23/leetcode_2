ca5741f5979af1297b3484007f79e29ce91877cc
dinglin <tim920323@gmail.com>
2020-06-22 21:05:59 -0400

    dsada

---
+++ TwoPointers/相向/TwoSumII.java
@@ -0,0 +1,25 @@
+//time: o(lgn)
+//space: o(1)
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers == null || numbers.length < 2) return  new int[]{};
+        int lo = 0, hi = numbers.length-1;
+
+        
+        while (lo < hi) {
+            int sum = numbers[lo] + numbers[hi];
+            if (sum == target) {
+                return new int[]{lo+1,hi+1};
+            }else if (sum > target) {
+                hi--;
+            }else {
+                lo++;
+            }
+        }
+        
+        return new int[]{lo+1,hi+1};
+    }
+}
+
+
