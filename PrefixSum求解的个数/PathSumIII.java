3e711ac88f2a39e3f0b7c6c8e58ee09fd5c64326
dinglin <tim920323@gmail.com>
2020-06-25 20:49:17 -0400

    dsada

---
+++ PrefixSum求解的个数/PathSumIII.java
@@ -0,0 +1,22 @@
+//time:O(n)
+//space:O(n)
+
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);  //Default sum = 0 has one count
+        return backtrack(root, 0, sum, map); 
+    }
+    //BackTrack one pass
+    public int backtrack(TreeNode root, int sum, int target, Map<Integer, Integer> map){
+        if(root == null)
+            return 0;
+        sum += root.val;
+        int res = map.getOrDefault(sum - target, 0);    //See if there is a subarray sum equals to target
+        map.put(sum, map.getOrDefault(sum, 0)+1);
+        //Extend to left and right child
+        res += backtrack(root.left, sum, target, map) + backtrack(root.right, sum, target, map);
+        map.put(sum, map.get(sum)-1);   //Remove the current node so it wont affect other path
+        return res;
+    }
+}
