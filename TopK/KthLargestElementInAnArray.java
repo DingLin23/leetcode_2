c94f022ad7a6d0e7bf1f4a4c1fad37a26c337221
dinglin <tim920323@gmail.com>
2020-06-25 21:09:28 -0400

    dasd

--- TopK/KthLargestElementInAnArray.java
+++ TopK/KthLargestElementInAnArray.java
@@ -8,3 +8,20 @@ class Solution {
         return nums[nums.length-k];
     }
 }
+
+//time:O(nlgk)
+//space:O(k)
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+         Queue<Integer> pq = new PriorityQueue<>();
+         for(int val : nums) {
+            pq.offer(val);
+
+            if(pq.size() > k) { //因为是第k个，所以只要比较比k多一个，最小的去掉。
+               pq.poll();
+            }
+        }
+    return pq.peek(); // min on the top, peek 就是答案
+    }
+}
