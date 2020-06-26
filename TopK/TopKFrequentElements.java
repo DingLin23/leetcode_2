13beb2ef9ff3e3ceb437559252161d7aa092c3ac
dinglin <tim920323@gmail.com>
2020-06-26 00:25:54 -0400

    dsada

--- TopK/TopKFrequentElements.java
+++ TopK/TopKFrequentElements.java
@@ -23,3 +23,38 @@ class Solution {
 
     }
 }
+
+
+    //algo: first we put num in nums into hashmap which will has num as key, freq as val. 
+    //then we creat a linked list names bucket add freq as index and num as val.
+    //so the most freq will be put the last index as go on. we call out
+
+    class Solution {
+        public int[] topKFrequent(int[] nums, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+        List<Integer>[] bucket = new List[nums.length + 1];
+        int[] res = new int[k];
+        
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0)+1);
+        }
+        
+        for (int key : map.keySet()) {
+            int freq = map.get(key);
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<>();
+            }
+            // this is why we start i = bucket.length - 1;
+            bucket[freq].add(key);
+        }
+        
+        for (int i = bucket.length - 1; i >= 0 && k > 0; i-- ) {
+            if (bucket[i] != null) {
+                for (int n : bucket[i]) {
+                    res[--k] = n;
+                }
+            }
+        }
+        return res;
+        }
+    }
