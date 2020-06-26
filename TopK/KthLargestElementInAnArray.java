ca42f306d67b671d3fb0290c1320fc8f388923fc
dinglin <tim920323@gmail.com>
2020-06-25 21:12:20 -0400

    dsad

--- TopK/KthLargestElementInAnArray.java
+++ TopK/KthLargestElementInAnArray.java
@@ -25,3 +25,38 @@ class Solution {
     return pq.peek(); // min on the top, peek 就是答案
     }
 }
+//time:o(n^2),best O(n)
+//space:o(1)
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        k = nums.length - k;
+       int lo = 0;
+       int hi = nums.length-1;
+       while (lo < hi) {
+           int pos = partition(nums, lo, hi);
+           if (pos == k) {
+               return nums[pos];
+           } else if (pos < k) {
+               lo = pos+1;
+           } else {
+               hi = pos-1;
+           }
+       }
+       return nums[lo];
+   }
+
+
+   private int partition(int[] nums, int lo, int hi) {
+       int pivot = nums[lo];
+       while (lo < hi) {
+           while (lo < hi && nums[hi] >= pivot) hi--;
+           nums[lo] = nums[hi];
+           while (lo < hi && nums[lo] <= pivot) lo++;
+           nums[hi] = nums[lo];
+       }
+       nums[lo] = pivot;
+       return lo;
+
+    }
+}
