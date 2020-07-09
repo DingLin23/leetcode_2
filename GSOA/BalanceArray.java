71ec8778ab7c25ad21ad979f89b32cfd553c1548
dinglin <tim920323@gmail.com>
2020-07-09 16:20:23 -0400

    打算

--- GSOA/BalanceArray.java
+++ GSOA/BalanceArray.java
@@ -10,7 +10,7 @@ public class BalanceArray {
             if (left_sum == sum - left_sum - arr[i]) {
                 return i;
             }
-            left_sum += arr[i];
+            left_sum += arr[i]; // 必须在这个位置，因为，先加这个的话，可能把pivot也加进去就不对了。
         }
         return -1;
     }
