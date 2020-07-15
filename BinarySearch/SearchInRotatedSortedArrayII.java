20d780dc98cb238fa1d0f67444fe45d63485f99e
dinglin <tim920323@gmail.com>
2020-07-15 13:33:42 -0400

    都撒上

--- BinarySearch/SearchInRotatedSortedArrayII.java
+++ BinarySearch/SearchInRotatedSortedArrayII.java
@@ -1,33 +1,30 @@
 // time:o(lgn)
 //space:o(1)
-class solution{
-public boolean search(int[] nums, int target) {
-    if (nums.length == 0) {
-        return false;
-    }
-    int lo = 0;
-    int hi = nums.length-1;
-    while (lo+1<hi) {
-        int mid = lo+(hi-lo)/2;
-        if (nums[mid] == target) {
-            return true;
-        }
-        if (nums[mid] == nums[hi]) {
-            hi--;
-        } else if (nums[mid] < nums[hi]) {
-            if (nums[mid] < target && target <= nums[hi]) {
-                lo = mid;
-            } else {
-                hi = mid;
+class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return false;
+        int left = 0, right = nums.length-1;
+        while (left+1 < right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] == target) return true;
+            if (nums[mid] == nums[right]) {
+                right--;
             }
-        } else {
-            if (nums[lo] <= target && target < nums[mid]) {
-                hi = mid;
-            } else {
-                lo = mid;
+            else if (nums[mid] < nums[right]) {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid;
+                }else {
+                    right = mid;
+                }
             }
-        }
+            else if (nums[mid] >= nums[left]) { //duplicate 有可能mid 和left是相等的，和基本版不同。 基本版没有重复可以不用写等于。
+                if (nums[mid] > target && target >= nums[left]) {
+                    right = mid;
+                }else {
+                    left = mid;
+                }
+            }
+        } 
+        return nums[left] == target || nums[right] == target;
     }
-    return nums[lo] == target || nums[hi] == target;
-}
 }
