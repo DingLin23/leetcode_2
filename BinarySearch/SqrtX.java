5f466defafca9419e1376370719f31a149526484
dinglin <tim920323@gmail.com>
2020-07-16 11:20:08 -0400

    dsad

---
+++ BinarySearch/SqrtX.java
@@ -0,0 +1,19 @@
+//time: o(lgn)
+//space: o(1)
+
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        int left = 0, right = x;
+        while (left+1 < right) {
+            int mid = left + (right - left)/2;
+            if (mid == x/mid) return mid;
+            else if (mid < x/mid) {
+                left = mid;
+            }else {
+                right = mid;
+            }
+        }
+        return (right == x/right)? right : left;
+    }
+}
