2113449389fb07c3cefec3289a54ef5ac803ec2b
dinglin <tim920323@gmail.com>
2020-07-15 21:18:21 -0400

    dsadas

--- BinarySearch/FindInMountainArray.java
+++ BinarySearch/FindInMountainArray.java
@@ -6,60 +6,48 @@
  *     public int length() {}
  * }
  */
- 
+ //time: o(lgn)
+ //space: o(1)
 class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
-          int peak = findPeakIndex(mountainArr);
-       int res = findTarget(mountainArr, target, 0, peak, true);
-       if (res != -1) {
-           return res;
-       } else {
-           return findTarget(mountainArr, target, peak+1, mountainArr.length()-1, false);
-       }
-   }
-
-   private int findPeakIndex(MountainArray A) {
-       int lo = 0;
-       int hi = A.length()-1;
-       while (lo+1<hi) {
-           int mid = lo+(hi-lo)/2;
-           if (A.get(mid) < A.get(mid+1)) {
-               lo = mid;
-           } else {
-               hi = mid;
-           }
-       }
-
-       return A.get(lo) > A.get(hi) ? lo : hi;
-   }
-
-   private int findTarget(MountainArray A, int target, int lo, int hi, boolean ascend) {
-       while (lo+1<hi) {
-           int mid = lo+(hi-lo)/2;
-           if (A.get(mid) == target) {
-               return mid;
-           } else {
-               if (ascend) {
-                   if (A.get(mid) < target) {
-                       lo = mid;
-                   } else {
-                       hi = mid;
-                   }
-               } else {
-                   if (A.get(mid) < target) {
-                       hi = mid;
-                   } else {
-                       lo = mid;
-                   }
-               }
-           }
-       }
-       if (A.get(lo) == target) {
-           return lo;
-       } else if (A.get(hi) == target) {
-           return hi;
-       } else {
-           return -1;
-       }
+        int left = 0, right = mountainArr.length()-1;
+        int peak = findPeak(target,mountainArr,left,right);
+        int res = findRes(target,mountainArr,left,peak,true);
+        if (res != -1) {
+            return res;
+        }else {
+            return findRes(target, mountainArr, peak+1, right,false);
+        }
+    }
+    
+    private int findPeak(int target,MountainArray A,int left, int right) {
+        while (left+1 < right) {
+            int mid = left + (right - left)/2;
+            if (A.get(mid) < A.get(mid+1)) {
+                left = mid;
+            }else {
+                right = mid;
+            }
+        }
+        return A.get(left) > A.get(right)? left : right;
+    }
+    private int findRes(int target,MountainArray A, int left, int right,boolean ascend) {
+        while (left+1 < right) {
+            int mid = left + (right - left)/2;
+            if (A.get(mid) == target) return mid; 
+            else {
+                if (ascend) {
+                    if (A.get(mid) < target) left = mid; // 因为山峰朝两边扩散变低。
+                    else right = mid;
+                }
+                else {
+                    if (A.get(mid) < target) right = mid;// 因为山峰朝两边扩散变低。
+                    else left = mid;
+                }
+            }
+        }
+        if (A.get(left) == target) return left;
+        if (A.get(right) == target) return right;
+        return -1;
     }
 }
