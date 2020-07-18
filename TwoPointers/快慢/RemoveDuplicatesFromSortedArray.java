26c78efa94ece7e3dc82b85560f015e429f0e1b3
dinglin <tim920323@gmail.com>
2020-07-17 21:18:50 -0400

    dasda

--- TwoPointers/快慢/RemoveDuplicatesFromSortedArray.java
+++ TwoPointers/快慢/RemoveDuplicatesFromSortedArray.java
@@ -1,11 +1,9 @@
 class Solution{
-public int removeDuplicates(int[] nums) {
-    int i = 0;
-    for (int num : nums) {
-        if (i < 1 || num != nums[i-1]) {
-            nums[i++] = num;
-        }
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int n : nums)
+            if (i == 0 || n > nums[i-1])
+                nums[i++] = n;
+        return i;
     }
-    return i;
-}
 }
