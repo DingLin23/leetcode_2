381b2bcee436f800abba066154b360bc68a88d9c
dinglin <tim920323@gmail.com>
2020-07-23 18:00:51 -0400

    都撒的撒

--- BFS/WallsAndGates.java
+++ BFS/WallsAndGates.java
@@ -1,5 +1,6 @@
 //DFS
 class Solution {
+        //根据答案来做dfs，bfs，取中间值如果有三个值。
     public void wallsAndGates(int[][] rooms) {
         for (int i = 0; i < rooms.length; i++) {
             for (int j = 0; j < rooms[i].length; j++) {
@@ -14,6 +15,8 @@ class Solution {
         if (i < 0 || j < 0 || i >= rooms.length || j >= rooms[i].length || rooms[i][j] == -1 || rooms[i][j] < distance) {
             // do not run dfs on out of boundry nodes, obstacle or another gate
             //rooms[i][j] < distance track 还没被遍历的cell，还有就是如果找最接近gate的cell的话就update。
+            //rooms[i][j] < d 小于d的话 rooms[i][j] 还没update的，只有大于d的情况下which inf 
+        // 如果小于d可能是-1，或者是已经遍历过且小于d，我们应该取小的。
             return;
         }
         rooms[i][j] = distance;
