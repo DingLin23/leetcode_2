a9549ecd5bd74334a3b8b6b682c5b2efb030a00d
dinglin <tim920323@gmail.com>
2020-07-25 21:14:47 -0400

    大大

---
+++ 特殊题型/Citadeloa.java
@@ -0,0 +1,33 @@
+import java.awt.List;
+
+public class Citadeloa {
+    public static int droppedRequests(List<Integer> requestTime) {
+        int request = 0, res = 0;
+        for (int i = 0; i < requestTime.size(); i++) {
+           while (i < requestTime.size() && requestTime.get(i) == 1) {
+               i++;
+               request++;
+               if (request > 3) {
+                   res++;
+               }
+           }
+           while (i < requestTime.size() && requestTime.get(i) < 10) {
+               i++;
+               request++;
+               if (request > 20) {
+                   res++;
+               }
+           }
+           while (i < requestTime.size() && requestTime.get(i) < 60) {
+            i++;
+            request++;
+            if (request > 60) {
+                res++;
+            }
+        }
+        }
+        return res;
+
+    }
+    
+}
