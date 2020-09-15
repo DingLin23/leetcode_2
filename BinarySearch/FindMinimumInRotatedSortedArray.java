a1fa4f0cda8bb6ede6dda744cf94e647c8daad9e
dinglin <tim920323@gmail.com>
2020-09-15 16:59:08 -0400

    打撒

--- BinarySearch/FindMinimumInRotatedSortedArray.java
+++ BinarySearch/FindMinimumInRotatedSortedArray.java
@@ -7,6 +7,8 @@ class Solution {
         int left = 0, right = nums.length-1;
         while (left+1 < right) {
             int mid = left + (right - left)/2;
+                //答案2分
+            //如果nums[mid] > nums[right] 说明答案在mid-> right这个区间。 otherwise在左区间
             if (nums[mid] > nums[right]) {
                     left = mid;
                 }else {
