0ce674320b1e5da42b003c8133f277a38297ee76
dinglin <tim920323@gmail.com>
2021-05-10 16:39:34 -0400

    saS

--- String/LongestCommonPrefix.java
+++ String/LongestCommonPrefix.java
@@ -17,7 +17,7 @@ public class LongestCommonPrefix {
         if (strs == null || strs.length == 0) return "";
         String res = strs[0];
         for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(res) != 0) {
+            while (strs[i].indexOf(res) != 0) { //or use startsWith() method
                 res = res.substring(0, res.length() - 1);
             }
         }
