34ac49821281f0c2b252b546208ebfffec0b7157
dinglin <tim920323@gmail.com>
2021-05-17 16:50:48 -0400

    dsada

---
+++ Sum系列/TwoSumII-InputArrayIsSorted.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+                // int res[] = new int[2];
+        if (numbers == null || numbers.length < 2) return  new int[]{};
+        int lo = 0, hi = numbers.length-1;
+
+        
+        while (lo < hi) {
+            int sum = numbers[lo] + numbers[hi];
+            if (sum == target) {
+                return new int[]{lo+1,hi+1};
+            }else if (sum > target) {
+                hi--;
+            }else {
+                lo++;
+            }
+        }
+        
+        return new int[]{lo+1,hi+1};
+    }
+}
